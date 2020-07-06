b25915c059cb2de95a782bb94133dd33373dc5aa
awpala <adam.w.pala@gmail.com>
2020-07-06 10:45:57 -0500

    Completed Part 1 Step 1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            imgURL: "",
+            productName: "",
+            productPrice: 0
+        }
+    }
+
+    // event handler functions
+    handleURL = (val) => {
+        this.setState({ imgURL: val} );
+    }
+
+    handleName = (val) => {
+        this.setState({ productName: val} );
+    }
+
+    handlePrice = (val) => {
+        this.setState({ productPrice: val} );
+    }
+
+    handleCancel = () => {
+        this.setState({
+            imgURL: "",
+            productName: "",
+            productPrice: 0
+        })
+    }
+    
     render() {
         return (
-            <div>Form</div>
+            <section className="form">
+                <input
+                    type={"text"}
+                    value={this.state.imgURL}
+                    onChange={e => this.handleURL(e.target.value)}
+                />
+                <input
+                    type={"text"}
+                    value={this.state.productName}
+                    onChange={e => this.handleName(e.target.value)}
+                />
+                <input
+                    type={"text"}
+                    value={this.state.productPrice}
+                    onChange={e => this.handlePrice(e.target.value)}
+                />
+                <button onClick={this.handleCancel}>
+                    Cancel
+                </button>
+                <button>Add to Inventory</button>
+            </section>
         );
     }
 }
